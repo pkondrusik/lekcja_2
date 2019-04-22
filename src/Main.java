eb35b795eb2998886053f40e2b8c44e1a4ca694e
Piotr Kondrusik <piotr.kondrusik@ttpsc.pl>
2019-04-22 16:54:24 +0200

    zad 8, chojna

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        zad1();
-//        zad2();
-//        zad3();
-//        zad4();
-//        zad5();
-//        zad6();
+        zad1();
+        zad2();
+        zad3();
+        zad4();
+        zad5();
+        zad6();
         zad7();
+        zad8();
     }
 
     public static void zad1(){
 
-        int dzien = 7;
+        int dzien = 3;
 
         switch (dzien){
             case 1: System.out.print("poniedzialek");
@@ -31,10 +32,8 @@ public class Main {
             case 7: System.out.print("niedziela");
                 break;
             default: System.out.print("bledny numer dnia!");
-
         }
-
-
+        System.out.println();
     }
 
     public static void zad2(){
@@ -59,9 +58,8 @@ public class Main {
             case "listopad": System.out.print("jesien");
                 break;
             default: System.out.print("nieprawidlowy miesiac");
-
         }
-
+        System.out.println();
     }
 
     public static void zad3(){
@@ -81,7 +79,7 @@ public class Main {
             break;
             default: System.out.print("nieznany znak!");
             }
-
+        System.out.println();
     }
 
     public static void zad4(){
@@ -91,37 +89,34 @@ public class Main {
         System.out.println(tablica[0]);
         System.out.println(tablica[1]);
         System.out.println(tablica[2]);
-
     }
 
     public static void zad5(){
 
-        int[] tablica1 = {1,2,3,4,5,6,7,8,9,0};
+        int[] tablica1 = {0,1,2,3,4,5,6,7,8,9};
         int[] tablica2 = new int[10];
         int j = 0;
-
 
         for (int i = tablica1.length; i > 0; i--){
             tablica2[j] = tablica1[i-1];
             j++;
-
         }
-
 
         for (int i = 0; i < tablica2.length; i++)
             System.out.print(tablica2[i] + " ");
 
-
+        System.out.println();
     }
 
     public static void zad6(){
+
         int n = 137;
         boolean pierwsza = true;
 
         for (int i = 2; i < n; i++)
             if (n % i == 0) pierwsza = false;
 
-        if (pierwsza==true)
+        if (pierwsza)
             System.out.println("pierwsza");
         else
             System.out.println("nie pierwsza");
@@ -129,13 +124,27 @@ public class Main {
 
 
     public static void zad7(){
-        int[][] tablica = new int [10][10];
+
+        int[][] tablica = new int[10][10];
 
         for (int i = 1; i < tablica.length+1; i++) {
             for (int j = 1; j < tablica[0].length+1; j++)
-                System.out.print((i) * (j) + " ");
-            System.out.print("\n");
+                System.out.print(i*j + " ");
+            System.out.println();
         }
     }
 
+    public static void zad8(){
+
+        int n = 3;
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n * 2; j++)
+                if (j < (n - i) || j > (n + i))
+                    System.out.print(" ");
+                else
+                    System.out.print("*");
+            System.out.println();
+        }
+    }
 }
